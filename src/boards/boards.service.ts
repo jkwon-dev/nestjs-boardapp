99b302491161d33704776b52cbcb5f6ae17e55bd
jkwon <kwonejk@gmail.com>
2021-10-11 23:13:09 +0800

    getAllBoard

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards = []; 
+
+    getAllBoards() {
+        return this.boards; 
+    }
+
+
+}
