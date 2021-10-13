aa7e138918fed1d2d55e7688c58e555c15292b20
jkwon <kwonejk@gmail.com>
2021-10-13 15:25:01 +0800

    deletebyid

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { throwIfEmpty } from 'rxjs';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -24,6 +25,9 @@ export class BoardsController {
         return this.boardsService.getBoardById(id); 
     }
 
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string): void {
+        this.boardsService.deleteBoard(id); 
+    }
 
-    
 }
