3a3da298375e21fd6fa91c56d5af2d6476baf73a
jkwon <kwonejk@gmail.com>
2021-10-16 08:14:24 +0800

    create user module and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'src/configs/typeorm.config';
 import { BoardsModule } from './boards/boards.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule],
+    BoardsModule,
+    AuthModule],
 })
 export class AppModule {}
